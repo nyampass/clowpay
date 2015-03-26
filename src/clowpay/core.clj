21da5069de3c8c75f838554a46bbd6569e385c85
OHTA Shogo <athos0220@gmail.com>
2015-03-26 15:19:38 +0900

    Add missing APIs

--- src/clowpay/core.clj
+++ src/clowpay/core.clj
@@ -1,11 +1,18 @@
 (ns clowpay.core
   (:import [jp.webpay.webpay WebPay]
-           [jp.webpay.webpay.data CardRequest TokenResponse CustomerResponse ChargeResponse]))
+           [jp.webpay.webpay.data CardRequest TokenResponse CustomerResponse
+            ChargeResponse ChargeResponseList RecursionResponse RecursionResponseList]
+           [java.util Date]))
 
 (set! *warn-on-reflection* true)
 
 (defn webpay [^String key]
   (WebPay. key))
+
+(defn unixtime ^long [int-or-date]
+  (if (integer? int-or-date)
+    (long int-or-date)
+    (quot (.getTime ^Date int-or-date) 1000)))
 
 (defn ^TokenResponse create-token [^WebPay webpay number exp-month exp-year cvc name
                                    & {:keys [uuid]}]
@@ -59,3 +66,71 @@
         uuid
         (.uuid ^String uuid))
       .execute))
+
+(defn ^ChargeResponse retrieve-charge [^WebPay webpay ^String charge-id]
+  (.. webpay charge (retrieve charge-id)))
+
+(defn retrieve-charges [^WebPay webpay
+                        & {:keys [count offset created customer-id shop recursion]}]
+  (let [charges (-> (.. webpay charge allRequest)
+                    (cond->
+                      count
+                      (.count (long count))
+                      offset
+                      (.offset (long offset))
+                      created
+                      (.created (unixtime created))
+                      customer-id
+                      (.customer ^String customer-id)
+                      shop
+                      (.shop ^String shop)
+                      recursion
+                      (.recursion recursion))
+                    .execute)]
+    (seq (.data ^ChargeResponseList charges))))
+
+(defn ^RecursionResponse create-recursion
+  [^WebPay webpay amount ^String customer-id period
+   & {:keys [currency shop description first-scheduled uuid]
+      :or {currency "jpy"}}]
+  (assert (contains? #{:month :year} period))
+  (-> (.. webpay recursion createRequest)
+      (.amount (long amount))
+      (.currency ^String currency)
+      (.customer customer-id)
+      (.period (name period))
+      (cond->
+        shop
+        (.shop ^String shop)
+        description
+        (.description ^String description)
+        first-scheduled
+        (.firstScheduled (unixtime first-scheduled))
+        uuid
+        (.uuid ^String uuid))
+      .execute))
+
+(defn ^RecursionResponse retrieve-recursion [^WebPay webpay ^String recursion-id]
+  (.. webpay recursion (retrieve recursion-id)))
+
+(defn ^RecursionResponse delete-recursion [^WebPay webpay ^String recursion-id]
+  (.. webpay recursion (delete recursion-id)))
+
+(defn retrieve-recursions [^WebPay webpay
+                           & {:keys [count offset created customer shop suspended]}]
+  (let [recursions (-> (.. webpay recursion allRequest)
+                       (cond->
+                         count
+                         (.count (long count))
+                         offset
+                         (.offset (long offset))
+                         created
+                         (.created (unixtime created))
+                         customer
+                         (.customer ^String customer)
+                         shop
+                         (.shop ^String shop)
+                         suspended
+                         (.suspended (boolean suspended)))
+                       .execute)]
+    (seq (.data ^RecursionResponseList recursions))))
