649362d3e2234938e9789cbc413a81f943e66757
OHTA Shogo <athos0220@gmail.com>
2015-03-26 16:49:27 +0900

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clowpay "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clowpay "0.1.1"
+  :description "WebPay API wrapper library for Clojure "
+  :url "https://github.com/nyampass/clowpay"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
